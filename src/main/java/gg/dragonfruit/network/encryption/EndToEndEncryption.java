704e0046872cba9a107ab21aa664e6a51f8853be
Jaiden <jaibrown51@gmail.com>
2022-06-21 21:55:21 +0100

    Fix encryption methods

--- src/main/java/gg/dragonfruit/network/encryption/EndToEndEncryption.java
+++ src/main/java/gg/dragonfruit/network/encryption/EndToEndEncryption.java
@@ -79,7 +79,7 @@ public class EndToEndEncryption {
      * @param otherPublicKey the other user's public key.
      * @return the encrypted string.
      */
-    public String encrypt(String string, BigInteger otherPublicKey) {
+    public String encrypt(String string) {
         return new BigInteger(string.getBytes())
                 .multiply(this.sharedKey).toString();
     }
@@ -91,7 +91,7 @@ public class EndToEndEncryption {
      * @param otherPublicKey the other user's public key.
      * @return the decrypted string.
      */
-    public String decrypt(String string, BigInteger otherPublicKey) {
+    public String decrypt(String string) {
         return new String(new BigInteger(string).divide(this.sharedKey)
                 .toByteArray());
     }
