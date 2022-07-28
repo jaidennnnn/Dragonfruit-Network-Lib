474650bd67b3adc77dc4af6bd8bb063ce60c3585
Jaiden <jaibrown51@gmail.com>
2022-07-28 22:43:08 +0100

    debug

--- src/main/java/gg/dragonfruit/network/packet/DHRequestPacket.java
+++ src/main/java/gg/dragonfruit/network/packet/DHRequestPacket.java
@@ -6,6 +6,7 @@ public class DHRequestPacket extends Packet {
 
     @Override
     public void received(Connection connection) {
+        System.out.println("DH key requested");
         connection.sendPacket(new DHPublicKeyPacket(
                 connection.getSelfEndToEndEncryption().getPublicKey()));
     }
