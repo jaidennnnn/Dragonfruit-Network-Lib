d7fc55e7cad456a732c8ccfff5abe52216790500
Jaiden <jaibrown51@gmail.com>
2022-07-29 14:57:36 +0100

    Packet compression

--- src/main/java/gg/dragonfruit/network/util/PacketUtil.java
+++ src/main/java/gg/dragonfruit/network/util/PacketUtil.java
@@ -7,8 +7,13 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import gg.dragonfruit.network.packet.Packet;
+import net.jpountz.lz4.LZ4Compressor;
+import net.jpountz.lz4.LZ4Factory;
+import net.jpountz.lz4.LZ4SafeDecompressor;
 
 public class PacketUtil {
+
+    static LZ4Factory factory = LZ4Factory.fastestInstance();
 
     public static byte[] serializePacket(Packet packet) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -17,11 +22,20 @@ public class PacketUtil {
         byte[] data = out.toByteArray();
         os.close();
         out.close();
+        final int decompressedLength = data.length;
+        LZ4Compressor compressor = factory.fastCompressor();
+        int maxCompressedLength = compressor.maxCompressedLength(decompressedLength);
+        byte[] compressed = new byte[maxCompressedLength];
+        compressor.compress(data, 0, decompressedLength, compressed, 0, maxCompressedLength);
         return data;
     }
 
-    public static Packet deserializePacket(byte[] data) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
+    public static Packet deserializePacket(byte[] data)
+            throws IOException, ClassNotFoundException {
+        LZ4SafeDecompressor decompressor = factory.safeDecompressor();
+        byte[] restored = new byte[0];
+        decompressor.decompress(data, restored);
+        ByteArrayInputStream in = new ByteArrayInputStream(restored);
         ObjectInputStream is = new ObjectInputStream(in);
         Packet packet = (Packet) is.readObject();
         is.close();
